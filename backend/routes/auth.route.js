bd08a570517486275e4b5a8e918454aaae07b4be
Nishant-jswl <152166858+Uknowme-h@users.noreply.github.com>
2024-09-24 03:07:34 +0545

    fixed imports

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import { login, logout, addbook, signup, verifyEmail, forgotPassword, resetPassword, checkAuth } from '../controllers/auth.controller.js';
-import { verifyToken } from '../middleware/verifytoken.js';
-
+import { verifyToken } from '../middleware/verifyToken.js';
 
 
 const router = express.Router();
